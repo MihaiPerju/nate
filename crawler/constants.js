833aa3f808729e6bbe720fb479b973dceb4e50f4
Michael Perju <mikeperju@gmail.com>
2021-07-07 21:45:55 +0300

    completed page automation in raw form

---
+++ crawler/constants.js
@@ -0,0 +1,8 @@
+module.exports={
+    "link":"https://nate-eu-west-1-prediction-test-webpages.s3-eu-west-1.amazonaws.com/tech-challenge/page1.html",
+    "city": "London",
+    "name": "nate",
+    "password": "07000000000",
+    "email": "nate@nate.tech",
+    "gender": "female"
+}
