e0ab3a62d2ff33077e49819f65f559ff3f3f5bea
Michael Perju <mikeperju@gmail.com>
2021-07-09 17:46:44 +0300

    added docs and uncommented all the tests for the submission

--- crawler/automation.test.js
+++ crawler/automation.test.js
@@ -24,116 +24,116 @@ const init = async()=>{
     return { browser, page }
 }
 
-// test("it should click the start button", async() => {
-//     const { browser, page }= await init()
+test("it should click the start button", async() => {
+    const { browser, page }= await init()
 
-//     await page.goto(link)
+    await page.goto(link)
 
-//     await clickStartButton(page)
-//     const hiddenLoading = await page.$$('#loading-msg[hidden]')
-//     expect(hiddenLoading.length).not.toBe(0)
+    await clickStartButton(page)
+    const hiddenLoading = await page.$$('#loading-msg[hidden]')
+    expect(hiddenLoading.length).not.toBe(0)
 
-//     await browser.close()
-// })
+    await browser.close()
+})
 
-// test("it should select the right city and add the corresponding attributes", async() => {
-//     const { browser, page } = await init()
+test("it should select the right city and add the corresponding attributes", async() => {
+    const { browser, page } = await init()
 
-//     await page.goto(link)
+    await page.goto(link)
 
-//     await clickStartButton(page)
-//     await selectCity(page)
+    await clickStartButton(page)
+    await selectCity(page)
 
-//     // find the html element that has the attribute added to it
-//     const selectedCity = await page.$$('span[nate-dict-key="London"]')
-//     expect(selectedCity).not.toBe(null)
+    // find the html element that has the attribute added to it
+    const selectedCity = await page.$$('span[nate-dict-key="London"]')
+    expect(selectedCity).not.toBe(null)
 
-//     await browser.close()
-// })
+    await browser.close()
+})
 
-// test("it should select the corresponding city depending on the constants", async() => {
-//     const { browser, page } = await init()
+test("it should select the corresponding city depending on the constants", async() => {
+    const { browser, page } = await init()
 
-//     await page.goto(link)
+    await page.goto(link)
 
-//     await clickStartButton(page)
+    await clickStartButton(page)
 
-//     // altering the constants
-//     values.city="Amsterdam"
+    // altering the constants
+    values.city="Amsterdam"
 
-//     await selectCity(page)
+    await selectCity(page)
 
-//     // find the html element that has the attribute added to it
-//     let selectedCity = await page.$$('span[nate-dict-key="Amsterdam"]')
-//     expect(selectedCity).not.toBe(null)
+    // find the html element that has the attribute added to it
+    let selectedCity = await page.$$('span[nate-dict-key="Amsterdam"]')
+    expect(selectedCity).not.toBe(null)
 
-//     values.city="New York"
+    values.city="New York"
 
-//     await selectCity(page)
+    await selectCity(page)
 
-//     // find the html element that has the attribute added to it
-//     selectedCity = await page.$$('span[nate-dict-key="New York"]')
-//     expect(selectedCity).not.toBe(null)
+    // find the html element that has the attribute added to it
+    selectedCity = await page.$$('span[nate-dict-key="New York"]')
+    expect(selectedCity).not.toBe(null)
 
-//     await browser.close()
-// })
+    await browser.close()
+})
 
-// test("it should not add an attribute if a city was not selected", async() => {
-//     const { browser, page } = await init()
+test("it should not add an attribute if a city was not selected", async() => {
+    const { browser, page } = await init()
 
-//     await page.goto(link)
-//     await clickStartButton(page)
+    await page.goto(link)
+    await clickStartButton(page)
 
-//     // find the html element that has the attribute added to it
-//     const selectedCity = await page.$$('span[nate-dict-key="London"]')
-//     expect(selectedCity.length).toBe(0)
+    // find the html element that has the attribute added to it
+    const selectedCity = await page.$$('span[nate-dict-key="London"]')
+    expect(selectedCity.length).toBe(0)
 
-//     await browser.close()
-// })
+    await browser.close()
+})
 
-// test("it should click on the select input and then click on the corresponding city, leaving the click attributes", async() => {
-//     const { browser, page } = await init()
+test("it should click on the select input and then click on the corresponding city, leaving the click attributes", async() => {
+    const { browser, page } = await init()
 
-//     await page.goto(link)
-//     await clickStartButton(page)
-//     await selectCity(page)
+    await page.goto(link)
+    await clickStartButton(page)
+    await selectCity(page)
 
-//     // find the html element that has the attribute added to it
-//     const clickedElements = await page.$$('span[nate-action-type="click"]')
-//     expect(clickedElements.length).toBe(2)
+    // find the html element that has the attribute added to it
+    const clickedElements = await page.$$('span[nate-action-type="click"]')
+    expect(clickedElements.length).toBe(2)
 
-//     await browser.close()
-// })
+    await browser.close()
+})
 
-// test("it should fill the form's inputs accordingly", async() => {
-//     const { browser, page } = await init()
+test("it should fill the form's inputs accordingly", async() => {
+    const { browser, page } = await init()
 
-//     await page.goto(link)
-//     await clickStartButton(page)
-//     await selectCity(page)
-//     // move to the next page
-//     await page.click("#next-page-btn")
+    await page.goto(link)
+    await clickStartButton(page)
+    await selectCity(page)
+    // move to the next page
+    await page.click("#next-page-btn")
 
-//     // initially, there are no completed inputs
-//     let nameInput = await page.$$('#name[nate-action-type="input"]')
-//     let pwdInput = await page.$$('#pwd[nate-action-type="input"]')
-//     let emailInput = await page.$$('#email[nate-action-type="input"]')
-//     expect(nameInput.length).toBe(0)
-//     expect(pwdInput.length).toBe(0)
-//     expect(emailInput.length).toBe(0)
+    // initially, there are no completed inputs
+    let nameInput = await page.$$('#name[nate-action-type="input"]')
+    let pwdInput = await page.$$('#pwd[nate-action-type="input"]')
+    let emailInput = await page.$$('#email[nate-action-type="input"]')
+    expect(nameInput.length).toBe(0)
+    expect(pwdInput.length).toBe(0)
+    expect(emailInput.length).toBe(0)
 
-//     await fillForm(page)
+    await fillForm(page)
 
-//     // find the html element that has the attribute added to it
-//     nameInput = await page.$$('#name[nate-action-type="input"]')
-//     pwdInput = await page.$$('#pwd[nate-action-type="input"]')
-//     emailInput = await page.$$('#email[nate-action-type="input"]')
-//     expect(nameInput.length).toBe(1)
-//     expect(pwdInput.length).toBe(1)
-//     expect(emailInput.length).toBe(1)
+    // find the html element that has the attribute added to it
+    nameInput = await page.$$('#name[nate-action-type="input"]')
+    pwdInput = await page.$$('#pwd[nate-action-type="input"]')
+    emailInput = await page.$$('#email[nate-action-type="input"]')
+    expect(nameInput.length).toBe(1)
+    expect(pwdInput.length).toBe(1)
+    expect(emailInput.length).toBe(1)
 
-//     await browser.close()
-// })
+    await browser.close()
+})
 
 test("it should fill the form's gender select accordingly", async() => {
     const { browser, page } = await init()
